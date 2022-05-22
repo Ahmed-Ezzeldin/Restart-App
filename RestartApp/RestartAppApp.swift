75de2c344da585e027661ee362c0f7fa90406c00
Ahmed <ahmed.ezzeldin.ibrahim@gmail.com>
2022-05-22 20:20:36 +0200

    Initial Commit

---
+++ RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by Codemaker on 22/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
