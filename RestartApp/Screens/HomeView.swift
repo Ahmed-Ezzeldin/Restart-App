e6b1bad8bacf770637a8657ffb21da7f7fed9f7a
Ahmed <ahmed.ezzeldin.ibrahim@gmail.com>
2022-05-25 20:42:28 +0200

    home screen

--- RestartApp/Screens/HomeView.swift
+++ RestartApp/Screens/HomeView.swift
@@ -13,14 +13,42 @@ struct HomeView: View {
     
     var body: some View {
         VStack(spacing: 20) {
-            Text("Home")
-                .font(.largeTitle)
-            Button(
-                action: {
-                    isOnboardingViewActive = true;
-                }){
-                    Text("Restart")
-                }
+            // MARK: - Header
+            Spacer()
+            ZStack {
+                CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.1)
+                Image("character-2")
+                    .resizable()
+                    .scaledToFit()
+                .padding()
+            }
+            
+            
+            // MARK: - Center
+            
+            Text("The time that leads to mastery is dependent on the intensity of our focus.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            // MARK: - Footer
+            
+            Spacer()
+            Button(action: {
+                isOnboardingViewActive = true
+            }){
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
+                Text("Restart")
+                    .font(.system(.title3,design: .rounded))
+                    .fontWeight(.bold)
+            }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
+            
         }
     }
 }
